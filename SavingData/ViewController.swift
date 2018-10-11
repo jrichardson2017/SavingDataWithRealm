1e6eeef42b02c3503b8a1db04f511018a5b08ae1
Justin <jrichardson@freshbooks.com>
2018-10-10 22:19:31 -0400

    Initial Commit

---
+++ SavingData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SavingData
+//
+//  Created by Justin Richardson on 2018-10-10.
+//  Copyright © 2018 Justin Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
